4b57b823b11a7e7415251696e386b543e43ff70a
eunkenlow <eunkenlow@gmail.com>
2016-09-27 17:57:09 +0100

    added bike and docking station spec

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,15 @@
+require 'docking_station'
+describe DockingStation do
+  before :each do
+    @station = DockingStation.new
+    @bike = @station.release_bike
+  end
+  it "should get a bike" do
+    station = DockingStation.new
+    expect(@bike).to be_an_instance_of Bike
+  end
+
+  it "should get a working bike" do
+    expect(@bike).to respond_to :working?
+  end
+end
