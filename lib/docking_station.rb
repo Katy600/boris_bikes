48a503b71cbfc038001bec91e0225b029f557853
LMCMLJ <mjcalvey2@gmail.com>
2016-09-28 16:19:31 +0100

    adds test for bike returning and reading out array of bikes. Solves both tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,18 @@
 require './lib/bike'
 class DockingStation
+
+  def initialize
+    @bike_rack = []
+  end
+
   def release_bike
     Bike.new
   end
 
-  def return_bike(bike)
-  	@returned_bike = bike
+  def return_bike(bike_name)
+    @bike_rack << (bike_name)
   end
+
+  attr_reader :bike_rack
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,12 +14,19 @@ describe DockingStation do
     expect(@bike).to be_working
   end
 
-  describe 'docking a bike' do 
+  describe 'docking a bike' do
     it "should return a bike" do
-      expect(@station).to respond_to :return_bike  
-    end   
-    it "add a new instance variable for bike" do
-      expect(@station.return_bike(@bike)).to eq(@bike)
+      expect(@station).to respond_to :return_bike
     end
+
+    it "add a new instance variable for bike" do
+      expect(@station.return_bike(@bike)).to eq([@bike])
+      expect(@station.bike_rack).to eq ([@bike])
+    end
+
+    it "reads array of stored bikes" do
+
+    end
+
   end
 end
