4943d0bed6d5cdbb30fb9136af39899a03cf04f0
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-30 15:44:35 +0100

    Double introduced step 19

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,52 +2,51 @@ require 'docking_station'
 
 describe DockingStation do
   subject(:docking_station) {described_class.new}
-
+  before :each do
+    
+    @bike = double(:bike)
+    @broken_bike = Bike.new(false)
+  end
   it { is_expected.to respond_to :release_bike }
 
   it "gets a bike and checks if it works" do
-    bike = Bike.new
-    expect(bike).to be_working   
+    expect(@bike).to be_working
   end
 
-  it "it is not releasing a broken bike" do 
-    broken_bike = Bike.new(false)
-    expect(broken_bike.working?).to eq(false)
-  end 
+  it "it is not releasing a broken bike" do
+    expect(@broken_bike.working?).to eq(false)
+  end
 
   it { is_expected.to respond_to :bikes}
 
   it { is_expected.to respond_to(:dock).with(1).arguments }
 
   it "returns docked bikes" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bikes).to eq [bike]
+    subject.dock(@bike)
+    expect(subject.bikes).to eq [@bike]
   end
   it 'returns faulty bike' do
-    bike_faulty = Bike.new(false)
-    expect(bike_faulty.working?).to eq(false) 
+    expect(@broken_bike.working?).to eq(false)
   end
 
-  it 'is going to seperate not working bikes from working bikes' do 
-      bike_faulty = Bike.new(false)
-    expect(subject.dock(bike_faulty)).to eq([bike_faulty])
+  it 'is going to seperate not working bikes from working bikes' do
+    expect(subject.dock(@broken_bike)).to eq([@broken_bike])
 
-  end 
+  end
 
-describe 'Error messages' do 
+describe 'Error messages' do
 
   it "doesn't release bikes when there aren't any available" do
     expect{ subject.release_bike }.to raise_error "NO BIKES!"
   end
 
   it "return an error when docking stations are 20" do
-    stub_const("DEFAULT_CAPACITY", 20)
-    DEFAULT_CAPACITY.times { subject.dock Bike.new }
-    expect { subject.dock Bike.new }.to raise_error "Docking Stations are full"
+    #stub_const("DEFAULT_CAPACITY", 20)
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock @bike }
+    expect { subject.dock @bike }.to raise_error "Docking Stations are full"
   end
 
-end 
+end
 
   it "returns capacity to be equal 15" do
     docking_station = DockingStation.new(15)
